07aa8f5bc2ab48cac8a1cbb16de248ae1eb8e5d6
primus11 <1196764+primus11@users.noreply.github.com>
2024-01-16 15:24:38 +0100

    rebased, fixed, added tests

--- src/sync/impl/synchronize.d.ts
+++ src/sync/impl/synchronize.d.ts
@@ -10,6 +10,7 @@ export default function synchronize({
   log,
   shouldUpdateRecord,
   conflictResolver,
+  pushShouldConfirmOnlyAccepted,
   _unsafeBatchPerCollection,
   unsafeTurbo,
 }: SyncArgs): Promise<void>
